c2edc6f817bbc7202f55eac5e57417a172fe51c3
Garvit Agrawal <81832341+garvitagrawal30@users.noreply.github.com>
2023-05-31 21:32:15 +0530

    Add files via upload

--- src/Components/Projects/projects.jsx
+++ src/Components/Projects/projects.jsx
@@ -1,25 +1,55 @@
 import React from 'react'
 import './projects.css'
-import Ecommerce from '../../img/ecommerce.png';
-import {Swiper, SwiperSlide} from 'swiper/react'
-import 'swiper/css'
+import ICG from '../../img/icg.png';
+import Sentiment from '../../img/sentiment.png';
+import Portfolio from '../../img/portfolio.png';
+
 const Projects = () => {
   return (
     <div className="projects" id='projects'>
       <div className="heading">
 
         <span>My Projects</span>
-        <span>Portfolio</span>
         </div>
+        <div className="list">
+        
+        <div className="sentiment">
+        <a href="https://github.com/garvitagrawal30/IPL_2022_Twitter_Sentiment_Analysis/blob/main/Analyser.ipynb" style={{textDecoration:'none'}}>
+        <div className="img1">
+          <img src={Sentiment} alt="" />
+          </div>
+          </a>
+          <span>IPL 2022 Tweets Sentiment Analyser</span>
+          
+        </div>
+        
 
-        <Swiper>
-            <SwiperSlide>
-                
+        
+        <div className="text1">
+          <span>A Machine Learning model that analyses the nature of a tweet using NLP and VADER Sentiment analyser.</span>
+        </div>
+        
+        <div className="icg">
+          <a href="https://github.com/garvitagrawal30/Image-Caption-Generator/blob/main/Generator.ipynb">
+          <div className="img2">
+          <img src={ICG} alt="" />
+          </div>
+          </a>
+          <span>Image Caption Generator</span>
 
-
-
-            </SwiperSlide>
-        </Swiper>
+        </div>
+        <div className="text2">
+          <span>A deep learning model that uses CNN and LSTM to generate an appropriate caption for an image.</span>
+        </div>
+        <div className="portfolio">
+          <img src={Portfolio} alt="" />
+          <span>Personal Portfolio</span>
+        </div>
+        <div className="text3">
+          <span>A Web Developement project developed using ReactJS and CSS.</span>
+        </div>
+        </div>
+     
     </div>
   )
 }
